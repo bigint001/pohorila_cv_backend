aee233d8624eda5a12bcbfd0d995f56114d4256f
ivanpohorilyyyyi <thepatrickmaps@gmai.com>
2025-08-22 18:04:41 +0300

    fix settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-47z0zo(=4bi-r5aprr!uck-h&&qyqpp1%0f!)(+la0@tbo+-(5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
