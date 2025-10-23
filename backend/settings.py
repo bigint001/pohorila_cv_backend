b36c0a1145dddf7ed2e77d8e41b38a491921cec6
IVAN ATOM1K <ivan.kuruch@urich.org>
2025-10-23 22:15:18 +0200

    Switch DB to Postgres via dj_database_url

--- backend/settings.py
+++ backend/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import os
+import dj_database_url
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,10 +79,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,   # держим соединение кэшированным
+        ssl_require=True    # для Render Postgres по HTTPS
+    )
 }
 
 # Password validation
